6d4ed7acf2ca3a4173a4c1623a346551ca94b69d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-22 15:05:39 +0200

    Controller URL is to be configured

--- ioc.go
+++ ioc.go
@@ -93,10 +93,13 @@ func completer(in prompt.Document) []prompt.Suggest {
 func main() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
+
+	controllerUrl = viper.GetString("CONTROLLER_URL")
 	p := prompt.New(executor, completer)
 	p.Run()
 }
