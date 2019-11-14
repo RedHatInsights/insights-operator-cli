1d97b183af617e6a567080592d61e2841805f4e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-14 10:34:13 +0100

    Documentation for all structures

--- ioc.go
+++ ioc.go
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// Implementation of command-line client for the insights operator instrumentation service.
 package main
 
 import (
@@ -42,15 +44,21 @@ var username string
 var password string
 var files []prompt.Suggest
 
-func tryToLogin(username string, password string) {
-	fmt.Println(Blue("\nDone"))
-}
-
+// Representation of cluster record in the controller service.
+//     ID: unique key
+//     Name: cluster GUID in the following format:
+//         c8590f31-e97e-4b85-b506-c45ce1911a12
 type Cluster struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 
+// Representation of configuration profile record in the controller service.
+//     ID: unique key
+//     Configuration: a JSON structure stored in a string
+//     ChangeAt: username of admin that created or updated the configuration
+//     ChangeBy: timestamp of the last configuration change
+//     Description: a string with any comment(s) about the configuration
 type ConfigurationProfile struct {
 	Id            int    `json:"id"`
 	Configuration string `json:"configuration"`
@@ -59,6 +67,14 @@ type ConfigurationProfile struct {
 	Description   string `json:"description"`
 }
 
+// Representation of cluster configuration record in the controller service.
+//     ID: unique key
+//     Cluster: cluster ID (not name)
+//     Configuration: a JSON structure stored in a string
+//     ChangeAt: timestamp of the last configuration change
+//     ChangeBy: username of admin that created or updated the configuration
+//     Active: flag indicating whether the configuration is active or not
+//     Reason: a string with any comment(s) about the cluster configuration
 type ClusterConfiguration struct {
 	Id            int    `json:"id"`
 	Cluster       string `json:"cluster"`
@@ -69,6 +85,17 @@ type ClusterConfiguration struct {
 	Reason        string `json:"reason"`
 }
 
+// Representation of trigger record in the controller service
+//     ID: unique key
+//     Type: ID of trigger type
+//     Cluster: cluster ID (not name)
+//     Reason: a string with any comment(s) about the trigger
+//     Link: link to any document with customer ACK with the trigger
+//     TriggeredAt: timestamp of the last configuration change
+//     TriggeredBy: username of admin that created or updated the trigger
+//     AckedAt: timestamp where the insights operator acked the trigger
+//     Parameters: parameters that needs to be pass to trigger code
+//     Active: flag indicating whether the trigger is still active or not
 type Trigger struct {
 	Id          int    `json:"id"`
 	Type        string `json:"type"`
@@ -80,6 +107,10 @@ type Trigger struct {
 	AckedAt     string `json:"acked_at"`
 	Parameters  string `json:"parameters"`
 	Active      int    `json:"active"`
+}
+
+func tryToLogin(username string, password string) {
+	fmt.Println(Blue("\nDone"))
 }
 
 func performReadRequest(url string) ([]byte, error) {
