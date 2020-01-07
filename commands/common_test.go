7dfef7597242ef2071f69999e55d995200b8eddd
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-07 12:47:45 +0100

    Removed unused code

--- commands/common_test.go
+++ commands/common_test.go
@@ -17,12 +17,8 @@ limitations under the License.
 package commands_test
 
 import (
-	"bytes"
 	"github.com/logrusorgru/aurora"
 	"github.com/redhatinsighs/insights-operator-cli/commands"
-	"io"
-	"os"
-	"sync"
 )
 
 // configureColorizer configures the Aurora colorizer. For tests
@@ -30,43 +26,4 @@ import (
 func configureColorizer() {
 	colorizer := aurora.NewAurora(false)
 	commands.SetColorizer(colorizer)
-}
-
-// captureStandardOutput captures the standard output for specified code
-// block and then returns the captured output.
-// Please see https://medium.com/@hau12a1/golang-capturing-log-println-and-fmt-println-output-770209c791b4
-// for further explanation how it works under the hood.
-//
-// TODO: put it into a dependent module
-func captureStandardOutput(function func()) (string, error) {
-	// backup of the real stdout
-	stdout := os.Stdout
-
-	// temporary replacement for stdout
-	reader, writer, err := os.Pipe()
-	if err != nil {
-		return "", err
-	}
-
-	// temporarily replace real Stdout by the mocked one
-	defer func() {
-		os.Stdout = stdout
-	}()
-	os.Stdout = writer
-
-	// channel with captured standard output
-	captured := make(chan string)
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		var buf bytes.Buffer
-		wg.Done()
-		io.Copy(&buf, reader)
-		captured <- buf.String()
-	}()
-	wg.Wait()
-	// provided function that (probably) prints something to standard output
-	function()
-	writer.Close()
-	return <-captured, nil
 }
