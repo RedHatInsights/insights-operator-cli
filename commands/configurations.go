87badb636126a3f67dfb8f5972cd769bfe7ab762
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-10 13:04:40 +0100

    Fixed typo

--- commands/configurations.go
+++ commands/configurations.go
@@ -35,7 +35,7 @@ func ListOfConfigurations(api restapi.API, filter string) {
 		return
 	}
 
-	fmt.Println(colorizer.Magenta("List of configuration for all clusters"))
+	fmt.Println(colorizer.Magenta("List of configurations for all clusters"))
 	fmt.Printf("%4s %4s %4s    %-20s %-20s %-10s %-12s %s\n", "#", "ID", "Profile", "Cluster", "Changed at", "Changed by", "Active", "Reason")
 	for i, configuration := range configurations {
 		// poor man's filtering
