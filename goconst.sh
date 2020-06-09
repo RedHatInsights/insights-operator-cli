af1e1d5bc0e85c360537f008a077bcc4f5d43449
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-09 10:44:30 +0200

    Colorized output of goconst.sh script

--- goconst.sh
+++ goconst.sh
@@ -13,6 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BLUE=$(tput setaf 4)
+RED_BG=$(tput setab 1)
+GREEN_BG=$(tput setab 2)
+NC=$(tput sgr0) # No Color
 
-go get github.com/jgautheron/goconst/cmd/goconst
-goconst ./...
+echo -e "${BLUE}Finding repeated strings that could be replaced by a constant${NC}"
+
+GO111MODULE=off go get github.com/jgautheron/goconst/cmd/goconst
+
+if [[ $(goconst -min-occurrences=2 ./... | tee /dev/tty | wc -l) -ne 0 ]]
+then
+    echo -e "${RED_BG}[FAIL]${NC} Duplicated string(s) found"
+    exit 1
+else
+    echo -e "${GREEN_BG}[OK]${NC} No duplicated strings found"
+    exit 0
+fi
