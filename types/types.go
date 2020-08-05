f9a8904a37fb8def97afc260a5644bfddeb43a81
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-05 10:10:12 +0200

    Package description for package types

---
+++ types/types.go
@@ -0,0 +1,21 @@
+/*
+Copyright © 2019, 2020 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package types contains all user-defined data types used in Insights Operator
+// CLI client. Some types that are the same as in other services (especially in
+// Insights Results Aggregator, Content Service) might be imported from (based
+// on) the common package insights-operator-utils/types.
+package types
