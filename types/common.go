39d21c393cca377c67d64360f038d7f8c6c3ccf8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-24 10:16:18 +0200

    Added links to generated documentation

--- types/common.go
+++ types/common.go
@@ -16,6 +16,12 @@ limitations under the License.
 
 package types
 
+// Generated documentation is available at:
+// https://godoc.org/github.com/RedHatInsights/insights-operator-cli/types
+//
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/insights-operator-cli/packages/types/common.html
+
 // Response structure represents default response of controller service to
 // different POST PUT requests.
 //     Status: status of response
