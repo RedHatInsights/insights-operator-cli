84877480b3859d1d021ab70a9ee5d01c731ab686
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-07-25 09:12:49 +0200

    More fixes

--- run_on_ci.sh
+++ run_on_ci.sh
@@ -15,11 +15,11 @@
 
 
 git clone https://github.com/RedHatInsights/insights-operator-controller.git
-cd insights-operator-controller
+cd insights-operator-controller || exit
 go build
 export ENV=test
 ./local_storage/create_database_sqlite.sh
 
 set -m
-./insights-operator-controller &bg
+./insights-operator-controller &
 sleep 5
